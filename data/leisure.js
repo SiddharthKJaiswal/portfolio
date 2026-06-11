549ad58bafce3a20d71b6c43ab248d3175d95d2d
Siddharth Jaiswal <siddkjaiswal@gmail.com>
2026-06-11 17:55:23 +0530

    Ball Hop and Dead Race

--- data/leisure.js
+++ data/leisure.js
@@ -34,6 +34,86 @@ const leisure = [
         "Unreal Engine",
         "C++"
     ]
+},
+
+{
+    id:"ballhop",
+
+    title:"Ball Hop",
+
+    role:"Personal Project",
+
+	links:
+	[
+		{
+			label: "Github",
+			url: "https://github.com/SiddharthKJaiswal/Public_BallHop"
+		},
+	
+		{
+			label: "Gameplay",
+			url: "https://www.youtube.com/watch?v=PaigGEr6M-A"
+		}
+	],
+    
+    image:"assets/images/ballhop.png",
+
+    description:
+    "A simple Unreal Engine interview task. Developed in Unreal Engine 5 using Blueprints.",
+
+    contributions:[
+		"Physics Interaction",
+		"Scoring System",
+		"Widget Integration",
+		"Save Load System",
+		"Map Traversal",
+    ],
+
+    technologies:[
+        "Unreal Engine",
+		"Save Load System"
+    ]
+},
+
+{
+    id:"deadrace",
+
+    title:"Dead Race",
+
+    role:"Personal Project",
+
+	links:
+	[
+		{
+			label: "Github",
+			url: "https://github.com/SiddharthKJaiswal/Public_DeadRace"
+		},
+	
+		{
+			label: "Gameplay",
+			url: "https://www.youtube.com/watch?v=PaigGEr6M-A"
+		}
+	],
+    
+    image:"assets/images/deadrace.png",
+
+    description:
+    "A simple Unreal Engine interview task. Developed in Unreal Engine 5 using Blueprints.",
+
+    contributions:[
+		"Chaos Vehicle",
+		"Mixamo Character",
+		"Pawn Sensing for basic NPC AI Behavior",
+		"Health, Death and Scoring Systems",
+		"Weapon, Projectile, Niagra",
+		"Procedural Runtime Corridor Creation",
+    ],
+
+    technologies:[
+        "Unreal Engine",
+		"Vehicle",
+		"Procedural Runtime Corridor" 
+    ]
 }
 
 ];
